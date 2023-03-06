ec7ae87e4b34e556fd87b7e58b6d93b5b9387df1
Grzegorz Cieslak <geger92@gmail.com>
2023-03-06 21:57:25 +0100

    Zadanie domowe - dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool female = true;
+int age = 33;
+
+if(female && age <30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if(name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if(!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
